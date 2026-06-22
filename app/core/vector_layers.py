ba584a6fde33c9e23be02a4ce3b1c3ca83f61190
pkubecek <kubecekpepa8@gmail.com>
2026-06-22 13:39:45 +0200

    Update vector_layers.py

--- app/core/vector_layers.py
+++ app/core/vector_layers.py
@@ -531,7 +531,7 @@ def add_vector_layers(
         # 503 - Silnice / zpevněná cesta
         cgdf = isom("503")
         mask_service = (c("highway").isin(["tertiary_link", "service"]) | c("highway").isin(["track", "road", "cycleway", "unclassified"]) &
-                        (c("surface").isin(["concrete", "asphalt"])) | c("tracktype" == "grade1") &
+                        (c("surface").isin(["concrete", "asphalt"])) | (c("tracktype") == "grade1") &
                         ~c("tunnel").isin(["yes", "avalanche_protector", "building_passage"]) &
                         (c("bridge") != "yes") & (c("access") != "private"))
         if cgdf is not None:
