192487f81f5c9893d447d110af23d2e0f4dd3ea7
pkubecek <kubecekpepa8@gmail.com>
2026-08-19 11:19:59 +0200

    Update vector_layers.py

--- backend/app/core/vector_layers.py
+++ backend/app/core/vector_layers.py
@@ -493,8 +493,8 @@ def add_vector_layers(
                 pm("sym501", 49, mask, zab("ArealUceloveZastavby"))
         else:
             pm("sym501", 49,
-               (c("amenity") == "parking") | (c("place") == "square") | (c("highway").isin(["pedestrian", "footway"]),
-               gdf_polys))
+               (c("amenity") == "parking") | (c("place") == "square") | c("highway").isin(["pedestrian", "footway"]),
+               gdf_polys)
 
         # 502D - Dálnice
         cgdf = isom("502D")
